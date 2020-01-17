36bf38c2bf1af5d23e47866e955d7ad6b3b2021f
dengyizheng <dengyizheng@xiaomi.com>
2020-01-17 17:33:13 +0800

    圆角动图控件

---
+++ app/src/main/java/com/example/uicomponents/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.uicomponents
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.uicomponents.cornergifview.CornerGifActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv_corner_gif.setOnClickListener {
+            Intent().apply {
+                setClass(this@MainActivity, CornerGifActivity::class.java)
+                startActivity(this)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/uicomponents/cornergifview/CornerGifActivity.kt
@@ -0,0 +1,17 @@
+package com.example.uicomponents.cornergifview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.uicomponents.R
+import kotlinx.android.synthetic.main.activity_corner_gif.*
+
+class CornerGifActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_corner_gif)
+        Glide.with(this)
+            .load("http://qn.niaogebiji.com/7339550725ac1cba9881a65.90582627.gif")
+            .into(iv_gif_corner)
+    }
+}
