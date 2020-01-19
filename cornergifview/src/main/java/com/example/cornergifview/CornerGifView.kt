e3173707d0f681e51223aa5c027d92b8ede4da89
dengyizheng <dengyizheng@xiaomi.com>
2020-01-19 14:48:06 +0800

    CornerGifView支持边框设置

--- cornergifview/src/main/java/com/example/cornergifview/CornerGifView.kt
+++ cornergifview/src/main/java/com/example/cornergifview/CornerGifView.kt
@@ -10,10 +10,15 @@ class CornerGifView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyle: Int = 0
 ) : GifImageView(context, attrs, defStyle) {
+
     private var mCornerColor = Color.WHITE
     private var mPaint: Paint? = null
+    private var mStrokePaint: Paint? = null
+    private var mStrokeCorner: Float? = 0F
     private var mPath: Path? = null
     private var mFloats: FloatArray? = null
+    private var mRectF: RectF? = null
+    private var mStrokeWidth = 0F
 
     init {
         init(context, attrs)
@@ -25,48 +30,78 @@ class CornerGifView @JvmOverloads constructor(
         val mRightTopCorner = a.getDimension(R.styleable.CornerGifView_rightTopCorner, 0f)
         val mLeftBottomCorner = a.getDimension(R.styleable.CornerGifView_leftBottomCorner, 0f)
         val mRightBottomCorner = a.getDimension(R.styleable.CornerGifView_rightBottomCorner, 0f)
+
+        mStrokeWidth = a.getDimension(R.styleable.CornerGifView_strokeWidth, 0f)
+        val strokeColor = a.getColor(R.styleable.CornerGifView_strokeColor, Color.WHITE)
+        val strokeCorner = a.getDimension(R.styleable.CornerGifView_strokeCorner, 0f)
+        mStrokeCorner = if (strokeCorner > 0) {
+            strokeCorner
+        } else {
+            mLeftTopCorner
+        }
+        mStrokePaint = Paint()
+        mStrokePaint?.color = strokeColor
+        mStrokePaint?.style = Paint.Style.STROKE
+        mStrokePaint?.strokeWidth = mStrokeWidth
+        mStrokePaint?.isAntiAlias = true
+
         mCornerColor = a.getColor(R.styleable.CornerGifView_cornerColor, Color.WHITE)
         a.recycle()
         mFloats = floatArrayOf(
-            mLeftTopCorner,
-            mLeftTopCorner,
-            mRightTopCorner,
-            mRightTopCorner,
-            mLeftBottomCorner,
-            mLeftBottomCorner,
-            mRightBottomCorner,
-            mRightBottomCorner
+            mLeftTopCorner, mLeftTopCorner, mRightTopCorner, mRightTopCorner,
+            mLeftBottomCorner, mLeftBottomCorner, mRightBottomCorner, mRightBottomCorner
         )
         mPath = Path()
-        mPath!!.fillType = Path.FillType.INVERSE_EVEN_ODD
-        mPath!!.addRoundRect(
+        mPath?.fillType = Path.FillType.INVERSE_EVEN_ODD
+        mPath?.addRoundRect(
             RectF(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat()),
             mFloats!!,
             Path.Direction.CCW
         )
         mPaint = Paint()
-        mPaint!!.style = Paint.Style.FILL_AND_STROKE
-        mPaint!!.color = mCornerColor
-        mPaint!!.isAntiAlias = true
+        mPaint?.style = if (mStrokeWidth > 0) {
+            Paint.Style.FILL
+        } else {
+            Paint.Style.FILL_AND_STROKE
+        }
+        mPaint?.color = mCornerColor
+        mPaint?.isAntiAlias = true
     }
 
+    /**
+     * 用于动态改变圆角颜色
+     * 例如父布局的背景色由服务器下发的数据决定时
+     * 可以调用此方法将圆角颜色设置为与父布局背景色一致
+     */
     fun setCornerColor(cornerColor: Int) {
         mCornerColor = cornerColor
-        mPaint!!.color = mCornerColor
+        mPaint?.color = mCornerColor
         invalidate()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldW: Int, oldH: Int) {
         super.onSizeChanged(w, h, oldW, oldH)
-        mPath!!.reset()
-        val mRectF = RectF(0f, 0f, w.toFloat(), h.toFloat())
-        mPath!!.addRoundRect(mRectF, mFloats!!, Path.Direction.CCW)
+        mPath?.reset()
+        mRectF = if (mStrokeWidth > 0) {
+            RectF(
+                0f + mStrokeWidth / 2,
+                0f + mStrokeWidth / 2,
+                w.toFloat() - mStrokeWidth / 2,
+                h.toFloat() - mStrokeWidth / 2
+            )
+        } else {
+            RectF(0f, 0f, w.toFloat(), h.toFloat())
+        }
+        mPath?.addRoundRect(mRectF!!, mFloats!!, Path.Direction.CCW)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.save()
         canvas.drawPath(mPath!!, mPaint!!)
+        if (mStrokeWidth > 0) {
+            canvas.drawRoundRect(mRectF!!, mStrokeCorner ?: 0F, mStrokeCorner ?: 0F, mStrokePaint!!)
+        }
         canvas.restore()
     }
 }
