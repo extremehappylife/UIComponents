719993f91d0f79d3f577ca72ff0459b54b40e201
zhangruirui <zhangruirui@xiaomi.com>
2020-02-14 16:48:21 +0800

    图片合成，支持按比例设置位置

--- app/src/main/java/com/happylife/uicomponents/CompositeActivity.kt
+++ app/src/main/java/com/happylife/uicomponents/CompositeActivity.kt
@@ -3,6 +3,7 @@ package com.happylife.uicomponents
 import android.app.Activity
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.text.TextUtils
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
@@ -36,6 +37,12 @@ class CompositeActivity : Activity() {
             })
         tv_composite.setOnClickListener {
             if (bm1 != null && bm2 != null) {
+                // 写死值
+                // CompositeUtil.setXYLabel(550, 520)
+                // 百分比
+                val x = if(!TextUtils.isEmpty(et_X.text)) (et_X.text.toString()).toFloat() else 0.0f
+                val y = if(!TextUtils.isEmpty(et_Y.text)) (et_Y.text.toString()).toFloat() else 0.0f
+                CompositeUtil.setXYLabelPer(x, y)
                 iv_composite_3.setImageBitmap(CompositeUtil.combineImages(bm1,bm2))
             }
         }
--- compositepicture/src/main/java/com/happylife/compositepicture/CompositeUtil.kt
+++ compositepicture/src/main/java/com/happylife/compositepicture/CompositeUtil.kt
@@ -3,6 +3,22 @@ package com.happylife.compositepicture
 import android.graphics.*
 
 object CompositeUtil {
+    var xLabel: Int = 0 // 具体数值
+    var yLabel: Int = 0
+
+    var xLabelPer: Float = 0.0f // 百分比
+    var yLabelPer: Float = 0.0f
+
+    fun setXYLabel(x: Int, y: Int) {
+        this.xLabel = x
+        this.yLabel = y
+    }
+
+    fun setXYLabelPer(x: Float, y: Float) {
+        this.xLabelPer = x
+        this.yLabelPer = y
+    }
+
     /**
      * 合成图片
      *
@@ -15,15 +31,34 @@ object CompositeUtil {
             return null
         }
         val bmp: Bitmap
+        val newBmp: Bitmap
         val width = if (first.width > second.width) first.width else second.width
         val height = if (first.height > second.height) first.height else second.height
         bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val paint = Paint()
-        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OVER)
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OVER) // 覆盖原图
+
+        val scaleWidth =
+            if (xLabelPer * width + second.width > first.width) (first.width - xLabelPer * width).toInt() else second.width
+        val scaleHeight =
+            if (yLabelPer * height + second.height > first.height) (first.height - yLabelPer * height).toInt() else second.height
+
+        newBmp = if (scaleWidth != second.width || scaleHeight != second.height) {
+            scaleBitmap(second, scaleWidth, scaleHeight)
+        } else {
+            second
+        }
+        // 第一种：如果超出大小，贴边（放置的位置可能不是初衷的位置）
+//        val leftPer = if (xLabelPer * width + second.width > first.width) (first.width - second.width).toFloat() else xLabelPer * width
+//        val topPer = if (yLabelPer * height + second.height > first.height) (first.height - second.height).toFloat() else yLabelPer * height
+
+        // 第二种：缩放图片（保留真实位置）
+        val leftPer = xLabelPer * width
+        val topPer = yLabelPer * height
 
         val canvas = Canvas(bmp)
         canvas.drawBitmap(first, 0f, 0f, null)
-        canvas.drawBitmap(second, 0f, 0f, paint)
+        canvas.drawBitmap(newBmp, leftPer, topPer, paint)
         return bmp
     }
 
